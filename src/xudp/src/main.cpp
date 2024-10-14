c5e0c39d41e64dcd96e16a732026f6361ce44edc
handabao <18862963156@163.com>
2024-10-14 10:16:29 +0800

    xudp 服务端

--- src/xudp/src/main.cpp
+++ src/xudp/src/main.cpp
@@ -1,4 +1,15 @@
-#include "XTCP.h"
+#ifdef _WIN32
+#include <winsock2.h>
+#define socklen_t int
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <arpa/inet.h> /// For inet_addr
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h> /// 包含 close 函数
+#define closesocket close
+#endif
 
 #include <cstring>
 #include <thread>
@@ -6,57 +17,61 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-class TcpThread
-{
-public:
-    /// 线程函数
-    void Main()
-    {
-        /// 处理客户端数据
-        char buffer[BUFFER_SIZE] = {0};
-
-        for (;;)
-        {
-            const int recvlen = client_xtcp_.recv(buffer, BUFFER_SIZE);
-            if (recvlen <= 0)
-            {
-                printf("Client disconnected or error occurred.\n");
-                break;
-            }
-            buffer[recvlen] = '\0';
-            if (strstr(buffer, "quit") != NULL)
-            {
-                printf("Client quit.\n");
-                client_xtcp_.send("quit success\n", 13);
-                break;
-            }
-            printf("Recv: %s\n", buffer);
-            client_xtcp_.send("ok\n", 3); /// Echo back
-        }
-
-        client_xtcp_.close();
-        delete this;
-    }
-
-public:
-    XTCP client_xtcp_;
-};
-
 
 int main(int argc, char *argv[])
 {
+#ifdef WIN32
+    WSADATA ws;
+    WSAStartup(MAKEWORD(2, 2), &ws);
+#endif
+
+    /// 客户端
     if (argc > 1)
     {
         if (strcmp(argv[1], "client") == 0)
         {
             printf("Client start.\n");
-            
         }
     }
-    else
+    else /// 服务端
     {
         printf("Server start.\n");
 
-        
+        int sock = ::socket(AF_INET, SOCK_DGRAM, 0);
+        if (sock <= 0)
+        {
+            printf("Create socket failed.\n");
+            return -1;
+        }
+
+        sockaddr_in saddr;
+        saddr.sin_family = AF_INET;
+        saddr.sin_port = htons(PORT);
+        saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        if (::bind(sock, (sockaddr *)&saddr, sizeof(saddr)) < 0)
+        {
+            printf("Bind port %d failed.\n", PORT);
+            return -2;
+        }
+
+        printf("Bind port %d success.\n", PORT);
+
+        ::listen(sock, 10);
+        sockaddr_in client;
+        socklen_t len = sizeof(client);
+        char buffer[BUFFER_SIZE] = {0};
+        int re_len = ::recvfrom(sock, buffer, sizeof(buffer), 0, (sockaddr *)&client, &len);
+        if (re_len <= 0)
+        {
+            printf("RecvFrom failed.\n");
+            return -3;
+        }
+        printf("Recv %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+        buffer[re_len] = '\0';
+        printf("%s\n", buffer);
+        ::sendto(sock, "67890", 5, 0, (sockaddr *)&client, sizeof(client));
     }
+    getchar();
 }
