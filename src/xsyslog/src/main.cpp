e8eef1aa476c595d98525a962b0e86844062df45
handabao <18862963156@163.com>
2024-10-18 17:59:30 +0800

    update regex

--- src/xsyslog/src/main.cpp
+++ src/xsyslog/src/main.cpp
@@ -4,7 +4,6 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-#include <iostream>
 #include <regex>
 #include <string>
 
@@ -27,14 +26,15 @@ int main(int argc, char *argv[])
             continue;
         }
         buf[len] = '\0';
-        // printf("Received: %s\n", buf);
+        printf("Received: %s\n", buf);
 
         /// <12>Oct 15 15:42:27 DSM918 Connection: User [hildness] from [240e:390:6e1:93a0:7c3b:9be8:5244:89b7] failed
         /// to sign in to [DSM] via [password] due to authorization failure.
+
         std::string src = buf;
         ///  正则表达式
         std::regex logRegex(
-            R"(<\d+>(\s*(.*?)\s\d+ \d+:\d+:\d+) ([a-zA-Z0-9]+) Connection: User \[([a-zA-Z0-9]+)] from \[(\b(?:(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){2})?\b)] failed to sign in to \[[a-zA-Z0-9]+] via \[password] due to authorization failure.)");
+            R"(<\d+>(\s*(?:.*?)\s\d+ \d+:\d+:\d+) ([a-zA-Z0-9]+) Connection: User \[([a-zA-Z0-9]+)\] from \[(\b(?:\d{1,3}\.){3}\d{1,3}|\b(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}\b)\])");
 
         std::smatch match;
         if (std::regex_search(src, match, logRegex))
@@ -44,15 +44,8 @@ int main(int argc, char *argv[])
             std::string user = match[3]; /// 用户名
             std::string ip = match[4]; /// IP 地址
 
-            /// 输出结果
-            std::cout << "Timestamp: " << timestamp << std::endl;
-            std::cout << "Device: " << device << std::endl;
-            std::cout << "User: " << user << std::endl;
-            std::cout << "IP: " << ip << std::endl;
-        }
-        else
-        {
-            std::cout << "No match found." << std::endl;
+
+            printf("Timestamp: [%s]\n Device: [%s]\n User: [%s]\n IP: %s\n  login failed!!!\n", timestamp.c_str());
         }
     }
     return 0;
